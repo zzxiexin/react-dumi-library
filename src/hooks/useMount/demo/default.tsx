29ced2654a5d99daa92268188f850b7c0e303514
zzxiexin <344391941@qq.com>
2024-02-22 21:10:17 +0800

    feat: 优化useMount案例

--- src/hooks/useMount/demo/default.tsx
+++ src/hooks/useMount/demo/default.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMount } from 'yxhooks';
 
 export default function App() {
-  const handleMessage = () => alert('message');
-  useMount(handleMessage);
-  return <div className="App">好戏开始了！</div>;
+  const [count, setCount] = useState(0);
+  useMount(() => setTimeout(() => setCount(count + 1), 2000));
+  return <div className="App">好戏开始了！count要变了 {count}</div>;
 }
